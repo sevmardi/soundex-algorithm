1d713d18b2fd952ac74e82fb377f237f8b6eea33
Sevak Mardirosian <smardirosians@gmail.com>
2018-03-21 20:48:27 +0100

    files to implement

---
+++ python/SoundEx.py
@@ -0,0 +1,22 @@
+import re
+import argparse
+
+
+class SoundEx:
+
+    def normalize(self, s):
+        pass
+
+    def algorithm(self, s):
+    	pass
+
+    def get_arguments(self, args):
+    	pass
+
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
